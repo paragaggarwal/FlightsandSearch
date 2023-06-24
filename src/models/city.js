09777a9123ffbabf236fba4435cfda7dda21f576
paragaggarwal <paragaggarwal04@gmail.com>
2023-06-24 18:06:24 +0530

    Added city service

--- src/models/city.js
+++ src/models/city.js
@@ -17,7 +17,6 @@ module.exports = (sequelize, DataTypes) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
-     
       unique: true,
     },
    
--- src/respository/city-repository.js
+++ src/respository/city-repository.js
@@ -1,9 +1,10 @@
 const { City }= require('../models/index');
 
 class CityRepository {
+
     async createCity({ name }) {
         try {
-            const city = await City.create({ name })
+            const city = await City.create({ name });
             return city;
         } catch (error) {
             console,log("Something went wrong in the repository layer");
